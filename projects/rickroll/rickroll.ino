6ac78bb09b5847719c4c2a42ac9cf3d8cd4e7892
mai <0hrs3g@gmail.com>
2025-02-13 15:06:38 -0800

    rm irrelevant LED

--- projects/rickroll/rickroll.ino
+++ projects/rickroll/rickroll.ino
@@ -1,7 +1,4 @@
-
-#define pin 8
-
-void song(int buzzerPin) {
+void roll(int buzzerPin) {
   tone(buzzerPin, 440);
   delay(129);
   noTone(buzzerPin);
@@ -187,56 +184,9 @@ void song(int buzzerPin) {
   noTone(buzzerPin);
 }
 
-// define pins (pwm)
-#define BLUE 3
-#define GREEN 5
-#define RED 6
-
+const int pin = 8;
 void setup() {
-    pinMode(RED, OUTPUT);
-    pinMode(GREEN, OUTPUT);
-    pinMode(BLUE, OUTPUT);
-    digitalWrite(RED, HIGH);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(BLUE, LOW);
-    song(pin);
+    roll(pin);
 }
 
-int redValue;
-int greenValue;
-int blueValue;
-
-void loop() {
-    #define delayTime 1 // fading time between colors
-    #define m 100 // max rgb value 1-255
-    redValue = m;
-    greenValue = 0;
-    blueValue = 0;
-
-    // red->green
-    for (int i = 0; i < m; ++i) {
-        --redValue;
-        ++greenValue;
-        analogWrite(RED, redValue);
-        analogWrite(GREEN, greenValue);
-        delay(delayTime);
-    }
-
-    // green->blue
-    for (int i = 0; i < m; ++i) {
-        --greenValue;
-        ++blueValue;
-        analogWrite(GREEN, greenValue);
-        analogWrite(BLUE, blueValue);
-        delay(delayTime);
-    }
-
-    // blue->red
-    for (int i = 0; i < m; ++i) {
-        --blueValue;
-        ++redValue;
-        analogWrite(BLUE, blueValue);
-        analogWrite(RED, redValue);
-        delay(delayTime);
-    }
-}
+void loop() {}
